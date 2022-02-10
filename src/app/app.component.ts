80f6f5ee4295645a32390cc2cd1ef72a23b4fb96
robert.boksa <robert.boksa@onwelo.com>
2022-02-10 21:53:49 +0100

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-list-udemy';
+  editMode = false;
+  taskName = 'Sugerowane zadanie codzienne: odkurzanie';
+  taskDate = '';
+  config: { [key: string]: string} | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Silownia',
+      deadline: '2020-01-02',
+      done: false
+    },
+    {
+      name: 'Basen',
+      deadline: '2021-02-07',
+      done: true
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2022-02-20',
+      done: false
+    },
+  ];
+
+    constructor() {
+      setTimeout(() => {
+        this.config = {
+        title: 'Lista zadań', 
+        footer: '© Lista zadań,All rights reserved.',
+        date: new Date().toDateString()
+      };
+      }, 500);
+    }  
+
+    clearTasks() {
+      this.tasks = [];
+    }
+
+    onKeyUp(event: KeyboardEvent){
+      const target = event.target as HTMLInputElement;
+      this.taskName = target.value; 
+    }
+
+    createTask(){
+      const task: Task = {
+      name: this.taskName,
+      deadline: this.taskDate,
+      done: false,
+      };
+      this.tasks.push(task);
+      this.taskName = '';
+      this.taskDate = '';
+    }
+
+    switchEditMode(){
+      this.editMode = !this.editMode;
+    }
+
 }
